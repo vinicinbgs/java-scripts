57ac9a8c1fe9f0d3fd31b6d5935b76e2f68bee97
Vinicius Dutra <dutra_morais@hotmail.com>
2021-10-31 00:23:32 -0300

    chore: add readme.md

--- ChallengerOne.java
+++ ChallengerOne.java
@@ -2,7 +2,7 @@ import java.util.*;
 
 public class ChallengerOne {
     public static void main(String[] args) {
-        int[] input = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int[] input = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
         System.out.println("Give an integer number: ");
 
@@ -12,7 +12,7 @@ public class ChallengerOne {
 
         sc.close();
 
-        if(k <= 0) {
+        if (k <= 0) {
             System.out.println("Give an integer number bigger then 0");
             return;
         }
@@ -30,7 +30,7 @@ public class ChallengerOne {
 
         // convert int[] to ArrayList<Integer>
         List<Integer> inputList = convertIntArrayToArrayList(input);
-        
+
         // to check if the position already have in result
         Map<Integer, Integer> alreadyHave = new HashMap<Integer, Integer>();
 
@@ -38,17 +38,17 @@ public class ChallengerOne {
         ArrayList<String> result = new ArrayList<String>();
 
         // O(n)
-        for(int i = 0; i < input.length; i++) {
-            if(k % input[i] == 0 && input[i] > 0) {
+        for (int i = 0; i < input.length; i++) {
+            if (k % input[i] == 0 && input[i] > 0) {
                 divisibleNumbers.put(k / input[i], i); // O(1)
             }
         }
 
         // O(n)
-        for(int j = 0; j < inputList.size(); j++) {
+        for (int j = 0; j < inputList.size(); j++) {
             Object idx = divisibleNumbers.get(inputList.get(j)); // O(1)
-            
-            if(idx != null && alreadyHave.get(idx) == null) {  // O(1)
+
+            if (idx != null && alreadyHave.get(idx) == null) { // O(1)
                 alreadyHave.put((Integer) idx, (Integer) idx); // O(1)
                 alreadyHave.put(j, j); // O(1)
                 result.add(Integer.toString(j) + " x " + idx); // O(1)
@@ -61,7 +61,7 @@ public class ChallengerOne {
     public static List<Integer> convertIntArrayToArrayList(int[] input) {
         List<Integer> inputList = new ArrayList<>(input.length);
 
-        for (int i: input) {
+        for (int i : input) {
             inputList.add(Integer.valueOf(i));
         }
 
---
+++ README.md
@@ -0,0 +1,9 @@
+## Big Tech - Challengers
+
+### How to run a script
+```bash
+javac ChallengerOne.java
+java ChallengerOne
+```
+
+#### 1) Challenger One - Given a integer number K and array Z return all key pairs that multiplied is equal to K.
